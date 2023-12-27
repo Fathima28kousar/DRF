2458e0882a5f588f6b809e276cc1431abaff58d0
Fathima28kousar <kousar28fathima@gmail.com>
2023-12-27 17:48:15 +0530

    Learning drf

---
+++ core/home/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+# Create your views here.
+
+
+
+@api_view(['GET'])
+def home(request):
+    student_objs = Student.objects.all()
+    serializer = StudentSerializer(student_objs, many=True)
+    return Response({'status' : 200, 'message' : serializer.data})
+
+@api_view(['POST'])
+def post_student(request):
+    data = request.data
+    serializer = StudentSerializer(data = request.data)
+
+    if not serializer.is_valid():
+        print(serializer.errors)
+        return Response({'status' : 403, 'errors': serializer.errors, 'message': 'Something went wrong'})
+    
+    serializer.save()
+    return Response({'status':200,'payload':data,'message':'Your data is saved'})
+    
